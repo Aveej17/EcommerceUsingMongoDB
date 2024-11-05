18426fce3fabb76085f05db6b07b7eacbd1fb772
Aveej17 <jeevaboobathi1718@gmail.com>
2024-11-05 23:01:12 +0530

    Implements Products CRUD oprations

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+
+      .then((result) => {
+        console.log("user: "+ result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((user) => {
+        // console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = User;
